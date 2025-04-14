7ab94c95114ec23f3ca4164a8283e3ee91e6f65d
AKTUBTERAJAN <rajankushwaha819@gmail.com>
2025-04-15 00:06:54 +0530

    Add new file Changes for Variable

--- MainFolder/InterShipProgram/Variable/Product2Num.java
+++ MainFolder/InterShipProgram/Variable/Product2Num.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 public class Product2Num {
     public static void main(String[] args) {
         try(Scanner sc = new Scanner(System.in)){
-            System.out.print("Enter The First Number:");
+            System.out.print("Enter The First Number: ");
             float a = sc.nextFloat();
-            System.out.print("Enter The First Number:");
+            System.out.print("Enter The Second   Number: ");
             float b = sc.nextFloat();
             float Product= a*b;
             System.out.println("Product of Two Number is :"+Product);
